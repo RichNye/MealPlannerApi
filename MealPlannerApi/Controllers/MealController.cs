c848c7e94bef33572cf8989b6792302d7f4ce2c5
RichNye <richardnye5@gmail.com>
2024-08-17 16:26:45 +0100

    Added delete all endpoint.

--- MealPlannerApi/Controllers/MealController.cs
+++ MealPlannerApi/Controllers/MealController.cs
@@ -13,7 +13,7 @@ namespace MealPlannerApi.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly MealService _mealService;
-        
+
 
         public MealController(ApplicationDbContext context, MealService mealService)
         {
@@ -46,7 +46,7 @@ namespace MealPlannerApi.Controllers
                 else
                 {
                     return Ok(meal);
-                }                
+                }
             }
             catch (Exception ex)
             {
@@ -72,10 +72,10 @@ namespace MealPlannerApi.Controllers
             // model must be valid if we've hit this point. Populate the full meal object.
             meal.CreatedDate = DateTime.UtcNow;
             meal.Name = mealDTO.Name;
-            if(mealDTO.Description != null)
+            if (mealDTO.Description != null)
             {
                 meal.Description = mealDTO.Description;
-            }           
+            }
 
             try
             {
@@ -89,6 +89,53 @@ namespace MealPlannerApi.Controllers
                 // log the error. Strip the exception out if production.
                 return StatusCode(StatusCodes.Status500InternalServerError, $"An error occurred while saving the meal: {ex}");
             }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Meal? meal = _context.Meals.Find(id);
+            if (meal != null)
+            {
+                try
+                {
+                    _context.Meals.Remove(meal);
+                    await _context.SaveChangesAsync();
+                    return Ok("meal deleted successfully.");
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex.Message);
+                }
+            }
+            else
+            {
+                return NotFound("Meal not found with that id.");
+            }
+        }
+
+        [HttpDelete("delete/all")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteAll()
+        {
+            try
+            {
+                List<Meal> meals = _context.Meals.ToList();
+                foreach(var meal in meals)
+                {
+                    _context.Remove(meal);
+                }
+
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch(Exception ex) 
+            {
+                return StatusCode(500, ex.Message);
+            }            
         }
     }
 }

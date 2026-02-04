2bc358838d74728bfd6ff87b0c3732c157cfde6a
RichNye <richardnye5@gmail.com>
2026-02-04 22:15:51 +0000

    Add first integration tests

---
+++ MealPlannerApi.Tests/RecipeTests.cs
@@ -0,0 +1,22 @@
+﻿using MealPlannerApi.Models;
+using NuGet.ContentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MealPlannerApi.Tests
+{
+    public class RecipeTests
+    {
+        [Fact]
+        public void Recipe_can_have_a_name()
+        {
+            var recipe = new Recipe
+            {
+                Name = "Spaghetti Bolognese"
+            };
+
+            Asset.Equals("Spaghetti Bolognese", recipe.Name);
+        }
+    }
+}
---
+++ MealPlannerApi.Tests/SanityTests.cs
@@ -0,0 +1,12 @@
+﻿using MealPlannerApi;
+
+namespace MealPlannerApi.Tests;
+
+public class SanityTests
+{
+    [Fact]
+    public void Tests_are_running()
+    {
+        Assert.True(true);
+    }
+}

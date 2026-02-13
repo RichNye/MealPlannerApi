402365dace69e6417ee65385a10d6b333727b479
RichNye <richardnye5@gmail.com>
2026-02-13 10:18:11 +0000

    Add new db migration for Identity

--- MealPlannerApi/Program.cs
+++ MealPlannerApi/Program.cs
@@ -1,6 +1,7 @@
 using MealPlannerApi.Data;
 using MealPlannerApi.Services;
 using MealPlannerApi.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace MealPlannerApi
@@ -12,7 +13,6 @@ namespace MealPlannerApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -20,19 +20,26 @@ namespace MealPlannerApi
             // Adds the new MealService for business logic for Meals
             builder.Services.AddScoped<IRecipeService, MealService>();
 
+            // add Identity and authorization
+            builder.Services.AddAuthorization();
+            builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
             // Configure the database connection
             if (builder.Environment.IsDevelopment())
             {
                 builder.Services.AddDbContext<ApplicationDbContext>
-                (options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+                (options => options.UseNpgsql(builder.Configuration.GetConnectionString("DevelopmentConnection")));
             }
             else
             {
                 builder.Services.AddDbContext<ApplicationDbContext>
                 (options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
             }
+
             var app = builder.Build();
             app.Logger.LogInformation("App started");
+            app.MapIdentityApi<IdentityUser>();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
